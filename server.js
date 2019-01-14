5cb3a8d8b75da94f63b507eaff9cc776a63f4714
wobblewedge <andobrown71@gmail.com>
2019-01-14 13:20:49 -0500

    added pug

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+//gives access to express api
 const express = require('express');
+const pug = require('pug');
+//instantiates
 const app = express();
-
+//request mappings
+app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+    //res.send('Hey there');
+    res.render('index');
 });
-
+//request mappings
 app.get('/home', (req, res) => {
     res.send('You did it!');
 });
-
+//connects to port and prints evidence in command line
 app.listen(3000, () => console.log('Gator app listening on port 3000!'));
