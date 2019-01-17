8df7ef5c9bbc4c5f15750b0d4267f4730f3b31fd
wobblewedge <andobrown71@gmail.com>
2019-01-17 10:28:23 -0500

    25percent there on voice

---
+++ middlewares/article.js
@@ -0,0 +1,9 @@
+//needs a variable that points to the url of the desired article. make html that has a button and shit.
+
+fetch(url)
+.then(function(response) {
+  // get body of article and assign it to a global? variable
+})
+.catch(function(error) {
+  console.log('Looks like there was a problem: \n', error);
+});
